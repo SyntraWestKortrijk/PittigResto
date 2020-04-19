64be583dc0fb82c1509b12429728d4396cd68cbd
Helena Coppieters <helenacoppieters@yahoo.com>
2020-04-19 14:48:37 +0200

    Add project files.

---
+++ Pittig/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Pittig.Models;
+using Pittig.Utility;
+
+namespace Pittig.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "Het {0} moet minstens {2} en maximimaal {1} tekens bevatten.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Wachtwoord")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Bevestig Wachtwoord")]
+            [Compare("Password", ErrorMessage = "Het wachtwoord en bevestiging van wachtwoord komen niet overeen")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Naam")]
+            public string Name { get; set; }
+            [Display(Name = "Straat")]
+            public string StreetAddress { get; set; }
+            [Display(Name = "TelNr")]
+            public string PhoneNumber { get; set; }
+            [Display(Name = "Gemeente")]
+            public  string City { get; set; }
+            [Display(Name = "Provincie")]
+            public string State { get; set; }
+            [Display(Name = "Postcode")]
+            public string PostalCode { get; set; }
+
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            string role = Request.Form["rdUserRole"].ToString();
+
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    Name = Input.Name,
+                    City=Input.City,
+                    StreetAddress=Input.StreetAddress,
+                    State=Input.State,
+                    PostalCode=Input.PostalCode,
+                    PhoneNumber=Input.PhoneNumber
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                   
+                    if(role==SD.KitchenUser)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.KitchenUser);
+                    }
+                    else
+                    {
+                        if (role == SD.FrontDeskUser)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.FrontDeskUser);
+                        }
+                        else
+                        {
+                            if (role == SD.ManagerUser)
+                            {
+                                await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                            }
+                            else
+                            {
+                                await _userManager.AddToRoleAsync(user, SD.CustomerEndUser);
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                return LocalRedirect(returnUrl);
+                            }
+                        }
+                    }
+
+                    return RedirectToAction("Index", "User", new { area = "Admin" });
+
+                    _logger.LogInformation("User created a new account with password.");
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    
+                  
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ Pittig/Models/OrderHeader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pittig.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        [Display(Name = "Datum bestelling")]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double   OrderTotalOriginal { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:00}")]
+        [Display(Name ="Totaal bestelling")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [Display(Name = "Tijdstip ophaling")]
+        public DateTime PickUpTime { get; set; }
+
+        [Required]
+        [NotMapped]
+        [Display(Name = "Datum ophaling")]
+        public DateTime PickUpDate { get; set; }
+
+       [Display(Name ="Kortingscode")]
+        public string CouponCode { get; set; }
+        [Display(Name = "Korting")]
+        public double CouponCodeDiscount { get; set; }
+        public string Status { get; set; }
+        [Display(Name = "Status betaling")]
+        public string PaymentStatus { get; set; }
+        [Display(Name = "Commentaar")]
+        public string Comments { get; set; }
+
+
+        [Display(Name = "Naam ophaler")]
+        public string PickupName { get; set; }
+
+        [Display(Name = "TelNr")]
+        public string PhoneNumber { get; set; }
+
+        public string TransactionId { get; set; }
+
+
+
+
+    }
+}
